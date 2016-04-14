3b5ec830b52dfea1e8484829e0ab7f709869a820
Zach Bruggeman <mail@bruggie.com>
2016-04-14 15:39:12 -0700

    Add various examples

---
+++ example/echo.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const Bot = require('../')
+
+let bot = new Bot({
+  token: 'PAGE_TOKEN',
+  verify: 'VERIFY_TOKEN'
+})
+
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+http.createServer(bot.middleware()).listen(3000)
