92187ae98ebec20aea01a56042a5372bc432586d
Stefan Bieliauskas <stefan.bie@live.de>
2017-06-26 17:03:55 +0200

    - add test for graph_url option

--- test/basic.js
+++ test/basic.js
@@ -19,6 +19,17 @@ tap.test('basic initialization', (t) => {
   t.end()
 })
 
+tap.test('initialization with changed graph_url ', (t) => {
+  let bot
+
+  t.doesNotThrow(() => { bot = new Bot({ token: 'foo', graph_url: 'http://example.com' }) }, 'creating bot does not throw')
+  t.type(bot, 'object', 'bot class is initiated correctly')
+  t.equals(bot.token, 'foo', 'bot token is stored correctly')
+  t.equals(bot.graph_url, 'http://example.com', 'bot graph_url is stored correctly')
+
+  t.end()
+})
+
 tap.test('missing token paramater', (t) => {
   t.throws(() => new Bot(), 'bot without token specified should throw')
   t.end()
